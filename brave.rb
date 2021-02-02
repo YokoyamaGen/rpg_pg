74ab7c613424c64c80571217e406c10dc0693ce0
YokoyamaGen <g.yokoyama.0616@gmail.com>
2021-02-02 19:41:41 +0900

    チャレンジ課題 ドラクエ風プログラム 指摘事項を反映

--- brave.rb
+++ brave.rb
@@ -3,8 +3,8 @@ require_relative "character.rb"
 class Brave < Character
   def attack(monster)
     damage = offense - monster.defense / 2
-    
-    puts "#{name}の攻撃！"
+
+    puts "#{@name}の攻撃！"
     puts "#{monster.name}に#{damage}のダメージを与えた！"
 
     monster.hp -= damage
--- monster.rb
+++ monster.rb
@@ -4,7 +4,7 @@ class Monster < Character
   def attack(brave)
     damage = offense - brave.defense / 2
 
-    puts "#{name}の攻撃！"
+    puts "#{@name}の攻撃！"
     puts "#{brave.name}は#{damage}のダメージを受けた！"
 
     brave.hp -= damage
